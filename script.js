fecf93eb62af09d19199978cd8da8e83a35787b7
MishaPrav2064 <186513797+MishaPrav2064@users.noreply.github.com>
2025-03-09 23:57:56 +0200

    Add color picker and fix bug with NaN in promot

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const styleContainer = window.getComputedStyle(container)
 const containerWidth = parseFloat(styleContainer.width)
 const containerHeight = parseFloat(styleContainer.height)
 
+const inputColor = document.querySelector(".inputColor")
+
 changeSize(16)
 
 const buttonSize = document.querySelectorAll(".buttonSize")
@@ -16,6 +18,8 @@ for (let i = 0; i < buttonSize.length; i++) {
         buttonSize[i].addEventListener("click",() => changeSize(16))
     } else if (sizeValue === "resetColor"){
         buttonSize[i].addEventListener("click",resetColor)
+    } else if (sizeValue === "squareBlack") {
+        buttonSize[i].addEventListener("click",() => switchToBlack)
     } else {
         buttonSize[i].addEventListener("click",() => changeSize(sizeValue))
     }
@@ -23,7 +27,7 @@ for (let i = 0; i < buttonSize.length; i++) {
 
 function customSize() {
     let newSize = parseInt(prompt("Enter custom size (Max 100)", ""));
-    if (newSize === undefined || newSize === null || newSize < 1 || newSize > 100) {
+    if (newSize === undefined || newSize === null || newSize < 1 || newSize > 100 || isNaN(newSize)) {
         alert("Error")
     } else {
         changeSize(newSize)
@@ -39,16 +43,35 @@ function changeSize(size) {
         square.style.height = (containerHeight / size) + "px";
         square.style.border = ((containerWidth / size) / 20) + "px solid lightgray";
         square.classList.add("square");
+        // square.addEventListener("mouseover", () => {
+        //     square.classList.add("hovered")
+        // })
         square.addEventListener("mouseover", () => {
-            square.classList.add("hovered")
-        })
+                square.style.backgroundColor = inputColor.value
+            })
 
         container.appendChild(square);
     }
 }
 
+// function resetColor() {
+//     const square = document.querySelectorAll(".square")
+//
+//     square.forEach((square) => square.classList.remove("hovered"))
+// }
+
 function resetColor() {
     const square = document.querySelectorAll(".square")
 
-    square.forEach((square) => square.classList.remove("hovered"))
+    square.forEach((square) => square.style.backgroundColor = "#ffffff")
 }
+
+// function switchToBlack() {
+//     const square = document.querySelectorAll(".square")
+//
+//     square.forEach(square => {
+//         if (window.getComputedStyle(square).backgroundColor !== "rgb(255, 255, 255)") {
+//             square.style.backgroundColor = "#000000"
+//         }
+//     })
+// }
